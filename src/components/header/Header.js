52e9422a01bf9b5d9167fc04b56453e6d2b44ce8
Iván Barrientos <ivan.barrientos@altia.es>
2020-10-02 09:27:58 +0200

    Cambios en el diseño del Header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -8,15 +8,14 @@ import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 import MenuIcon from '@material-ui/icons/Menu';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import CreateIcon from '@material-ui/icons/Create';
 
 
 const useStyles = makeStyles({
   list: {
-    width: 250,
-  },
-  fullList: {
-    width: 'auto',
-  },
+    width: 300,
+  }
 });
 
 function Header(props) {
@@ -36,26 +35,39 @@ function Header(props) {
 
   const list = (anchor) => (
     <div
-      className={clsx(classes.list, {
-        [classes.fullList]: anchor === 'top' || anchor === 'bottom',
-      })}
+      className={clsx(classes.list)}
       role="presentation"
       onClick={toggleDrawer(anchor, false)}
       onKeyDown={toggleDrawer(anchor, false)}
     >
-      <List>
-        <span className="header__icon item"></span>
-        <span className="header__name item">Formación</span>
+      <List className="top">
+        <span className="header__icon"></span>
+        <span className="header__name">Formación</span>
       </List>
       <Divider />
       <List>
-        <div className="item">Listado cursos</div>
-        <div className="item">Inscripciones</div>
+        <div className="item">
+          <span className="icon">
+            <CreateIcon></CreateIcon>
+          </span>
+          Listado cursos
+        </div>
+        <div className="item">
+          <span className="icon">
+            <CreateIcon></CreateIcon>
+          </span>
+            Inscripciones
+        </div>
       </List>
       <List>
         <div className="item">Visualizar cursos abiertos</div>
         <div className="item">Recomendarme cursos para mi perfil</div>
       </List>
+      <div className="bottom item">
+        <span className="icon">
+          <ExitToAppIcon></ExitToAppIcon>
+        </span>
+        <span>Cerrar sesión</span></div>
     </div>
   );
 
@@ -64,7 +76,7 @@ function Header(props) {
       {userInfo && (
         <Button className="" onClick={toggleDrawer('left', true)}><MenuIcon /></Button>)}
       <div>
-        <div className="header">
+        <div className="header header_center">
           <span className="header__icon"></span>
           <span className="header__name">Formación</span>
         </div>
