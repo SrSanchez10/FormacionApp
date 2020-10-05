122f2aa9d34939081fb8f83788f251f71e953a43
Álvaro Sanchez González <alvaro.sanchez@altia.loc>
2020-10-05 08:22:23 +0200

    Ejemplos en los inputs del formulario añadidos

--- src/modules/addCourse/AddCourse.js
+++ src/modules/addCourse/AddCourse.js
@@ -13,15 +13,15 @@ function AddCourse(props) {
         }}>
         <div className="item-list">
           <span className="add_course_span">Nombre: </span>
-          <input className="add_course_input"></input>
+          <input className="add_course_input" placeholder="Por Ejemplo: Comenzando con React"></input>
           <span className="add_course_span">Cupo de plazas: </span>
-          <input className="add_course_input"></input>
-          <span className="add_course_span">Fecha de inicio: </span>
-          <input className="add_course_input"></input>
+          <input className="add_course_input " placeholder="Por Ejemplo: 55"></input>
+          <span className="add_course_span" >Fecha de inicio: </span>
+          <input className="add_course_input" placeholder="Por Ejemplo: 05/10/2020"></input>
           <span className="add_course_span">Fecha de fin: </span>
-          <input className="add_course_input"></input>
+          <input className="add_course_input" placeholder="Por Ejemplo: 08/10/2020"></input>
           <span className="add_course_span">Horas: </span>
-          <input className="add_course_input"></input>
+          <input className="add_course_input" placeholder="Por Ejemplo: 24"></input>
           <span className="add_course_span">Responsable: </span>
           <input className="add_course_input"></input>
           <h1>{userInfo.rol}</h1>
