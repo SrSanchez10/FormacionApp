e81204fedc9788d4d344a595ff0a6772d8292d93
Álvaro Sanchez González <alvaro.sanchez@altia.loc>
2020-10-05 09:36:54 +0200

    Campos del formulario completados

--- src/modules/addCourse/AddCourse.js
+++ src/modules/addCourse/AddCourse.js
@@ -14,16 +14,37 @@ function AddCourse(props) {
         <div className="item-list">
           <span className="add_course_span">Nombre: </span>
           <input className="add_course_input" placeholder="Por Ejemplo: Comenzando con React"></input>
-          <span className="add_course_span">Cupo de plazas: </span>
+          <span className="add_course_span">Descripción: </span>
+          <input className="add_course_input" placeholder="Por Ejemplo: En este curso, vamos a realizar..."></input>
+          <span className="add_course_span">Índice de contenidos: </span>
+          <input className="add_course_input" placeholder="Por Ejemplo: Hello world en React"></input>
+          <span className="add_course_span">Tags(temáticas): </span>
+          <input className="add_course_input" placeholder="Por Ejemplo: #react"></input>
+          <span className="add_course_span">Cupo de plazas (máximo): </span>
           <input className="add_course_input " placeholder="Por Ejemplo: 55"></input>
+          <span className="add_course_span">Cupo de plazas (mínimo): </span>
+          <input className="add_course_input " placeholder="Por Ejemplo: 25"></input>
+          <span className="add_course_span">Objetivos del curso: </span>
+          <input className="add_course_input " placeholder="Por Ejemplo: Adquirir conocimientos básicos de React"></input>
           <span className="add_course_span" >Fecha de inicio: </span>
           <input className="add_course_input" placeholder="Por Ejemplo: 05/10/2020"></input>
           <span className="add_course_span">Fecha de fin: </span>
           <input className="add_course_input" placeholder="Por Ejemplo: 08/10/2020"></input>
+          <span className="add_course_span">Horario: </span>
+          <input className="add_course_input"></input>
           <span className="add_course_span">Horas: </span>
           <input className="add_course_input" placeholder="Por Ejemplo: 24"></input>
-          <span className="add_course_span">Responsable: </span>
-          <input className="add_course_input"></input>
+          <span className="add_course_span">Oficina a la pertenece: </span>
+          <input className="add_course_input " placeholder="Por Ejemplo: A Coruña"></input>
+          <span className="add_course_span">Tipo de formación: </span>
+          <input className="add_course_input " placeholder="Por Ejemplo: presencial/teleformación/mixta"></input>
+          <label className="add_course_label" for="formacion">Elija formación:</label>
+
+          <select className="add_course_select" name="formacion">
+            <option value="especifica">Específica</option>
+            <option value="estrategica">Estratégica</option>
+          </select>
+
           <h1>{userInfo.rol}</h1>
 
           <button
